e2f9fa6a1506198bcb940c71e6606694cba18b62
Andrii Manevych <amanevych@iteratorshq.com>
2023-05-31 14:36:40 +0200

    fixed bug with ignoreLogs

--- template/app/utils/helpers/ignoreWarnings.ts
+++ template/app/utils/helpers/ignoreWarnings.ts
@@ -4,7 +4,25 @@
  */
 import { LogBox } from "react-native"
 
+const IGNORED_LOGS = ["Require cycle:"]
+
 // prettier-ignore
-LogBox.ignoreLogs([
-  "Require cycle:",
-])
+LogBox.ignoreLogs(IGNORED_LOGS)
+
+// Workaround for Expo 48
+if (__DEV__) {
+  const withoutIgnored =
+    (logger) =>
+    (...args) => {
+      const output = args.join(" ")
+
+      if (!IGNORED_LOGS.some((log) => output.includes(log))) {
+        logger(...args)
+      }
+    }
+
+  console.log = withoutIgnored(console.log)
+  console.info = withoutIgnored(console.info)
+  console.warn = withoutIgnored(console.warn)
+  console.error = withoutIgnored(console.error)
+}
