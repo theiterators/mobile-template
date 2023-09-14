aaa55db6a42b284709d054d746f7ee27e1f11b71
manevych <ktars1324@gmail.com>
2023-09-14 09:47:48 +0200

    update react native to v0.72.4 and expo to v49.0.0

--- template/metro.config.js
+++ template/metro.config.js
@@ -1,4 +1,5 @@
-const { getDefaultConfig } = require("metro-config")
+const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config")
+
 const { getDefaultConfig: getDefaultExpoConfig } = require("@expo/metro-config")
 
 let metroConfig
@@ -11,44 +12,25 @@ try {
     Constants.executionEnvironment === "storeClient"
 } catch {}
 
+/**
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const config = {}
+
 if (isExpo) {
   /**
-   *  Expo metro config
-   * Learn more https://docs.expo.io/guides/customizing-metro
+     *  Expo metro config
+     * Learn more https://docs.expo.io/guides/customizing-metro
 
-   * For one idea on how to support symlinks in Expo, see:
-   * https://github.com/infinitered/ignite/issues/1904#issuecomment-1054535068
-   */
+    * For one idea on how to support symlinks in Expo, see:
+    * https://github.com/infinitered/ignite/issues/1904#issuecomment-1054535068
+    */
   metroConfig = getDefaultExpoConfig(__dirname)
 } else {
-  /**
-   * Vanilla metro config - we're using a custom metro config because we want to support symlinks
-   * out of the box. This allows you to use pnpm and/or play better in a monorepo.
-   *
-   * You can safely delete this file and remove @rnx-kit/metro-* if you're not
-   * using PNPM or monorepo or symlinks at all.
-   *
-   * However, it doesn't hurt to have it either.
-   */
-  const { makeMetroConfig } = require("@rnx-kit/metro-config")
-  const MetroSymlinksResolver = require("@rnx-kit/metro-resolver-symlinks")
-
-  metroConfig = (async () => {
-    const defaultConfig = await getDefaultConfig()
-    return makeMetroConfig({
-      projectRoot: __dirname,
-      // watchFolders: [`${__dirname}/../..`], // for monorepos
-      resolver: {
-        /**
-         * This custom resolver is for if you're using symlinks.
-         *
-         * You can disable it if you're not using pnpm or a monorepo or symlinks.
-         */
-        resolveRequest: MetroSymlinksResolver(),
-        assetExts: [...defaultConfig.resolver.assetExts, "bin"],
-      },
-    })
-  })()
+  metroConfig = getDefaultConfig(__dirname)
 }
 
-module.exports = metroConfig
+module.exports = mergeConfig(metroConfig, config)
